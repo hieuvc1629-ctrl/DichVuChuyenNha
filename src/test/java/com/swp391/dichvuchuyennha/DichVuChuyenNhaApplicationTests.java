8f894f050a9cfcdcb6b8662265093410419ca94b
bvd512004 <ducbach512k4@gmail.com>
2025-09-08 20:19:47 +0700

    create repo

---
+++ src/test/java/com/swp391/dichvuchuyennha/DichVuChuyenNhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.dichvuchuyennha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DichVuChuyenNhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
