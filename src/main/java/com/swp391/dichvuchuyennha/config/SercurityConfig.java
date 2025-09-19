d70528ac059958b8b40d52d966faf2d0f02c8619
bvd512004 <ducbach512k4@gmail.com>
2025-09-19 20:35:37 +0700

    Update package

---
+++ src/main/java/com/swp391/dichvuchuyennha/config/SercurityConfig.java
@@ -0,0 +1,4 @@
+package com.swp391.dichvuchuyennha.config;
+
+public class SercurityConfig {
+}
---
+++ src/main/java/com/swp391/dichvuchuyennha/exception/AppException.java
@@ -0,0 +1,7 @@
+package com.swp391.dichvuchuyennha.exception;
+
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+}
