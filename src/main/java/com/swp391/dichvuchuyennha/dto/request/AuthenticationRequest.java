dd4a73460c1672d85918d5a8bedcee84a44d60a6
bvd512004 <ducbach512k4@gmail.com>
2025-10-09 16:16:40 +0700

    Fix conflict about login

--- src/main/java/com/swp391/dichvuchuyennha/dto/request/AuthenticationRequest.java
+++ src/main/java/com/swp391/dichvuchuyennha/dto/request/AuthenticationRequest.java
@@ -9,7 +9,6 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthenticationRequest {
-    String username;
     String email;
     String password;
 }
