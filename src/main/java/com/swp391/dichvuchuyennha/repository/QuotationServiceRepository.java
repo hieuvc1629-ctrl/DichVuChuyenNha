91d4db19100d3e5aae261edd60fd4df7f9670205
bvd512004 <ducbach512k4@gmail.com>
2025-10-13 17:18:57 +0700

    Merge and and Fix conflict (remember pull code before you commit something to update new code from master)

--- src/main/java/com/swp391/dichvuchuyennha/repository/QuotationServiceRepository.java
+++ src/main/java/com/swp391/dichvuchuyennha/repository/QuotationServiceRepository.java
@@ -5,6 +5,8 @@ import com.swp391.dichvuchuyennha.entity.QuotationServices;
 import com.swp391.dichvuchuyennha.entity.Quotations;
 import com.swp391.dichvuchuyennha.entity.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,6 +20,8 @@ public interface QuotationServiceRepository extends JpaRepository<QuotationServi
             Services service,
             Prices price
     );
-//    List<QuotationServices> findByQuotation_Survey_Request_UserIdAndStatus(Integer userId);
+    
+//    @Query("SELECT qs FROM QuotationServices qs WHERE qs.quotation.survey.request.user.userId = :userId AND qs.status = :status")
+//    List<QuotationServices> findByQuotation_Survey_Request_UserIdAndStatus(@Param("userId") Integer userId, @Param("status") String status);
 
 }
--- src/main/java/com/swp391/dichvuchuyennha/service/QuotationForCustomerService.java
+++ src/main/java/com/swp391/dichvuchuyennha/service/QuotationForCustomerService.java
@@ -1,22 +1,22 @@
-//package com.swp391.dichvuchuyennha.service;
-//
-//import com.swp391.dichvuchuyennha.dto.response.QuotationForCustomer;
-//import com.swp391.dichvuchuyennha.entity.QuotationServices;
-//import com.swp391.dichvuchuyennha.entity.Users;
-//import com.swp391.dichvuchuyennha.mapper.QuotationServiceMapper;
-//import com.swp391.dichvuchuyennha.repository.QuotationServiceRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//@Service
-//@RequiredArgsConstructor
-//public class QuotationForCustomerService {
-//    private final QuotationServiceRepository quotationServiceRepository;
-//    private final QuotationServiceMapper quotationServiceMapper;
+package com.swp391.dichvuchuyennha.service;
+
+import com.swp391.dichvuchuyennha.dto.response.QuotationForCustomer;
+import com.swp391.dichvuchuyennha.entity.QuotationServices;
+import com.swp391.dichvuchuyennha.entity.Users;
+import com.swp391.dichvuchuyennha.mapper.QuotationServiceMapper;
+import com.swp391.dichvuchuyennha.repository.QuotationServiceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class QuotationForCustomerService {
+    private final QuotationServiceRepository quotationServiceRepository;
+    private final QuotationServiceMapper quotationServiceMapper;
 //    public List<QuotationForCustomer> getAllQuotationPending(Integer userId){
-//List<QuotationServices> quotationServices = quotationServiceRepository.findByQuotation_Survey_Request_UserIdAndStatus(userId);
+//    List<QuotationServices> quotationServices = quotationServiceRepository.findByQuotation_Survey_Request_UserIdAndStatus(userId);
 //
 //
 //    }
-//}
+}
