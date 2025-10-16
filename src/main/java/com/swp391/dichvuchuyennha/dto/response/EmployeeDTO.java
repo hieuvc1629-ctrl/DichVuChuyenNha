ec59b5c8af1031ca22217944310ee5b2522b4740
NguyenHai204 <haindhe181833@fpt.edu.vn>
2025-10-16 10:32:20 +0700

    fix assign vaf wworkprogress

--- src/main/java/com/swp391/dichvuchuyennha/dto/response/EmployeeDTO.java
+++ src/main/java/com/swp391/dichvuchuyennha/dto/response/EmployeeDTO.java
@@ -7,15 +7,11 @@ import lombok.Data;
 @AllArgsConstructor
 public class EmployeeDTO {
     private Integer employeeId;
-<<<<<<< HEAD
 
     private String username;
 
     private String position;
 
-=======
-    private String username;
-    private String position;
->>>>>>> 50d3ace9be4f20731befc6c501569bf0a7dae5b1
+
 }
 // employee sau khi bỏ của dũng
